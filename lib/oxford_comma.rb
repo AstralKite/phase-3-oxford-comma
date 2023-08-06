a72c5cf7191ab55fdc4184f7b496d5fafc7299bd
Fausto Ramirez <ramirezmedia@gmail.com>
2023-08-06 15:56:01 -0400

    commit tree faeccbbcbc342244cc03de66896eca3171124606

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
+    
+    if array.length == 1
+        return array.join
+    elsif array.length == 2
+        joinedString = array.join(" and ")
+        return joinedString;
+    else
+        lastItem = array.pop;
+        joinedString = array.join(", ")
 
+        return joinedString + ", and " + lastItem;
+    end
 end
+
+puts oxford_comma(["this", "word"])
+
+puts oxford_comma(["this", "word", "more"])
+
+puts oxford_comma(["this", "word", "more", "test", "wild"])
+
+puts oxford_comma(["fiddleheads", "okra", "kohlrabi"])
